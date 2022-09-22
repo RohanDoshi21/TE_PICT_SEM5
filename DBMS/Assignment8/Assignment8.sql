2a5e2a777eeb0cb93be79960ae95e5709b968a99
Rohan Doshi <rohan.doshi02@gmail.com>
2022-09-22 10:08:06 +0530

    Update Assignment8.sql

--- DBMS/Assignment8/Assignment8.sql
+++ DBMS/Assignment8/Assignment8.sql
@@ -7,134 +7,143 @@ import java.util.*;
 
 public class App {
 
-	public static void insert(int id,
-			int salary,
-			String firstname) throws SQLException {
-		String sql;
+    public static void insert(int id, int salary, String firstname) throws SQLException {
+        String sql;
 
-		sql = "INSERT into EMP values (" + Integer.toString(id) + ", " + Integer.toString(salary) + ", \"" + firstname
-				+ "\")";
+        sql = "INSERT into EMP values (" + Integer.toString(id) + ", " + Integer.toString(salary) + ", \"" + firstname + "\")";
 
-		stmt.executeUpdate(sql);
-	}
+        stmt.executeUpdate(sql);
+    }
 
-	public static void update(int id,
-			int salary) throws SQLException {
-		String sql;
+    public static void update(int id, int salary) throws SQLException {
+        String sql;
 
-		sql = "Update EMP SET salary = " + Integer.toString(salary) + " WHERE Emp_id = " + Integer.toString(id);
-		System.out.println(sql);
-		stmt.executeUpdate(sql);
-	}
+        sql = "Update EMP SET salary = " + Integer.toString(salary) + " WHERE Emp_id = " + Integer.toString(id);
 
-	public static void delete(int id) throws SQLException {
-		String sql;
+        stmt.executeUpdate(sql);
+    }
 
-		sql = "Delete FROM EMP WHERE Emp_id = " + Integer.toString(id);
-		stmt.executeUpdate(sql);
-	}
+    public static void delete(int id) throws SQLException {
+        String sql;
 
-	public static void read() throws SQLException {
-		String sql;
-		int id = 0;
-		int salary = 0;
-		String firstname = "";
+        sql = "Delete FROM EMP WHERE Emp_id = " + Integer.toString(id);
 
-		sql = "SELECT * FROM EMP";
-		ResultSet rs = stmt.executeQuery(sql);
+        stmt.executeUpdate(sql);
+    }
 
-		while (rs.next()) {
+    public static void read() throws SQLException {
+        String sql;
+        int id = 0;
+        int salary = 0;
+        String firstname = "";
 
-			id = rs.getInt("Emp_id");
-			salary = rs.getInt("salary");
-			firstname = rs.getString("fname");
+        sql = "SELECT * FROM EMP";
+        ResultSet rs = stmt.executeQuery(sql);
 
-			System.out.print("ID: " + id);
-			System.out.print(", Age: " + salary);
-			System.out.print(", First Name: " + firstname);
-		}
+        while (rs.next()) {
 
-		rs.close();
-	}
+            id = rs.getInt("Emp_id");
+            salary = rs.getInt("salary");
+            firstname = rs.getString("fname");
 
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://10.10.12.108:3306/t31126db";
+            System.out.print("ID: " + id);
+            System.out.print(", Age: " + salary);
+            System.out.print(", First Name: " + firstname);
+            System.out.println();
+        }
 
-	static final String USER = "t31126";
-	static final String PASS = "t31126";
-	static Connection conn = null;
-	static Statement stmt = null;
+        rs.close();
+    }
 
-	public static void main(String[] args) {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://10.10.12.108:3306/t31126db";
 
-		try {
+    static final String USER = "t31126";
+    static final String PASS = "t31126";
+    static Connection conn = null;
+    static Statement stmt = null;
 
-			Class.forName("com.mysql.cj.jdbc.Driver");
+    public static void main(String[] args) {
 
-			System.out.println("Connecting to database...");
-			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        try {
 
-			System.out.println("Creating statement...");
-			stmt = conn.createStatement();
-			int id = 0;
-			int salary = 0;
-			String firstname = "";
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-			Scanner sc = new Scanner(System.in);
-			boolean loop = true;
-			while (loop) {
-				System.out.println("\n----MENU----");
-				System.out.println("1.Read");
-				System.out.println("2.Insert");
-				System.out.println("3.Update");
-				System.out.println("4.Delete");
-				System.out.println("-1.Exit");
-				Integer choice = sc.nextInt();
-				switch (choice) {
-					case 1:
-						read();
-						break;
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
-					case 2:
-						insert(id, salary, firstname);
-						break;
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            int id = 0;
+            int salary = 0;
+            String firstname = "";
 
-					case 3:
-						update(id, salary);
-						break;
+            Scanner sc = new Scanner(System.in);
+            boolean loop = true;
+            while (loop) {
+                System.out.println("\n----MENU----");
+                System.out.println("1.Read");
+                System.out.println("2.Insert");
+                System.out.println("3.Update");
+                System.out.println("4.Delete");
+                System.out.println("-1.Exit");
+                Integer choice = sc.nextInt();
+                switch (choice) {
+                    case 1:
+                        read();
+                        break;
 
-					case 4:
-						delete(id);
-						break;
-					case -1:
-						System.out.println("Thak You");
-						sc.close();
-						loop = false;
-				}
-			}
+                    case 2:
+                        System.out.println("Enter ID");
+                        id = sc.nextInt();
+                        System.out.println("Enter Salary");
+                        salary = sc.nextInt();
+                        System.out.println("Enter Name");
+                        firstname = sc.next();
+                        insert(id, salary, firstname);
+                        break;
 
-			stmt.close();
-			conn.close();
-		} catch (SQLException se) {
+                    case 3:
+                        System.out.println("Enter ID");
+                        id = sc.nextInt();
+                        System.out.println("Enter Salary");
+                        salary = sc.nextInt();
+                        update(id, salary);
+                        break;
 
-			se.printStackTrace();
-		} catch (Exception e) {
+                    case 4:
+                        System.out.println("Enter ID");
+                        id = sc.nextInt();
+                        delete(id);
+                        break;
+                    case -1:
+                        System.out.println("Thak You");
+                        sc.close();
+                        loop = false;
+                }
+            }
 
-			e.printStackTrace();
-		} finally {
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
 
-			try {
-				if (stmt != null)
-					stmt.close();
-			} catch (SQLException se2) {
-			}
-			try {
-				if (conn != null)
-					conn.close();
-			} catch (SQLException se) {
-				se.printStackTrace();
-			}
-		}
-		System.out.println("Goodbye!");
-	}
+            se.printStackTrace();
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        } finally {
+
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {}
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Goodbye!");
+    }
 }
