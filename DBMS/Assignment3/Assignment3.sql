79d87fd090c26c5b09ebf9e1e7314df7b71a2424
Rohan Doshi <rohan.doshi02@gmail.com>
2022-09-28 22:50:49 +0530

    Update Assignment3.sql

--- DBMS/Assignment3/Assignment3.sql
+++ DBMS/Assignment3/Assignment3.sql
@@ -14,7 +14,20 @@ select dname, fname, lname, designation from Employee natural join Project natur
 select designation, Dept_id from Employee natural join Project natural join Dept where Pcost>30000;
 
 /* 6 */
-
+select Pname from Project where year(Pyear)=2015;
 
 /* 7 */
+select dname, Count(*) from Dept natural join Employee group by Dept_id;
 
+/* 8 */
+select year(JoinDate), Count(*) from Employee group by JoinDate;
+
+/* 9 */
+create view EMP_DEP as select * from Employee natural join Dept;
+select * from EMP_DEP;
+
+/* 10 */
+create view EMP_DETAILS as select Emp_id, fname, lname from Employee;
+select * from EMP_DETAILS;
+insert into EMP_DETAILS values (20, 'Sanika', 'Inamdar');
+drop view EMP_DETAILS;
