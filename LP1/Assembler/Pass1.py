10c5157b9ce50a4d46e52805b5befe4d3bd2d4c5
Rohan Doshi <rohan.doshi02@gmail.com>
2022-11-19 19:30:38 +0530

    Only Basic Condition

---
+++ LP1/Assembler/Pass1.py
@@ -0,0 +1,121 @@
+OPTAB = {
+    "STOP": "(IS,00)",
+    "ADD": "(IS,01)",
+    "SUB": "(IS,02)",
+    "MUL": "(IS,03)",
+    "MOVER": "(IS,04)",
+    "MOVEM": "(IS,05)",
+    "COMP": "(IS,06)",
+    "BC": "(IS,07)",
+    "DIV": "(IS,08)",
+    "READ": "(IS,09)",
+    "PRINT": "(IS,10)",
+    # "DC": "DL,01",
+    # "DS": "DL,02",
+    # "START": "AD,01",
+    # "STOP": "AD,02",
+    # "ORIGIN": "AD,03",
+    # "EQU": "AD,04",
+    # "LTORG": "AD,05",
+}
+
+REG = {
+    "AREG": "(1)",
+    "BREG": "(2)",
+    "CREG": "(3)",
+    "DREG": "(4)",
+}
+
+CC = {
+    "LT": "(1)",
+    "LE": "(2)",
+    "EQ": "(3)",
+    "GT": "(4)",
+    "GE": "(5)",
+    "ANY": "(6)"
+}
+
+
+class Pass1:
+    def __init__(self) -> None:
+        self.symtab = []
+        self.lc = 0
+
+    def updateSymtab(self, pair):
+        for i in self.symtab:
+            if i[0] == pair[0]:
+                i[1] = pair[1]
+        else:
+            self.symtab.append(pair)
+
+    def getSymtabLC(self, symbol):
+        for i in self.symtab:
+            if i[0] == symbol:
+                return i[1]
+        else:
+            return -1
+
+    def getSymtabPos(self, symbol):
+        for pos, val in enumerate(self.symtab):
+            if val[0] == symbol:
+                return pos
+        else:
+            return -1
+
+    def process(self):
+        with open("Simple.txt", "r") as file:
+            data = file.readlines()
+            for line in data:
+                word = line.replace('\n', '').split('\t')
+
+                if word[0] != '':  # A label is present
+                    pair = [word[0], self.lc]
+                    self.updateSymtab(pair)
+
+                if word[1] == 'START':
+                    self.lc = int(word[2])
+                    print(f"(AD,01)\t(C,{self.lc})")
+
+                if word[1] == 'DC':
+                    self.lc += 1
+                    # Change -1 according to syntax of input '19' -> -1   '19  -> 0
+                    constant = word[2][1:-1]
+                    print(f"(DL, 01)\t(C,{constant})")
+
+                if word[1] == 'DS':
+                    constant = int(word[2])
+                    self.lc += constant
+                    print(f"(DL, 02)\t(C,{constant})")
+
+                if OPTAB.get(word[1]) != None:
+                    code = OPTAB.get(word[1]) + "\t"
+
+
+                    j = 2
+                    while j < len(word):
+                        if REG.get(word[j]) != None:
+                            code += REG.get(word[j]) + "\t"
+                        elif CC.get(word[j]) != None:
+                            code += CC.get(word[j]) + "\t"
+                        else:
+                            # TODO: Literals
+                            if self.getSymtabLC(word[j]) == -1:
+                                pair = [word[j], -1]
+                                self.updateSymtab(pair)
+
+                            const = self.getSymtabPos(word[j])
+                            code += f"(S, {const})"
+
+                        j += 1
+
+                    print(code)
+
+                if word[1] == 'END':
+                    # TODO: Process literals
+                    print('(AD, 02)')
+
+            print(self.symtab)
+
+
+test = Pass1()
+test.process()
